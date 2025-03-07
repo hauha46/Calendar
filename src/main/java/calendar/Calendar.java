368f763dffdfb0a658d505db701ac3a97e5a26a3
Howie <hau.vu@minttown.jp>
2025-03-07 14:00:03 -0500

    Updated changes with recurring days

--- src/main/java/calendar/Calendar.java
+++ src/main/java/calendar/Calendar.java
@@ -2,6 +2,7 @@ package calendar;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
@@ -12,40 +13,134 @@ public class Calendar {
   private boolean autoDeclineConflicts = false;
 
   public Calendar() {
-    calendar = new TreeMap<>(Comparator.comparing((String date) -> LocalDate.parse(date)));
+    calendar = new TreeMap<>(Comparator.comparing((String date) -> LocalDate.parse(date, dateFormatter)));
   }
 
   public void setAutoDeclineConflicts(boolean autoDeclineConflicts) {
     this.autoDeclineConflicts = autoDeclineConflicts;
   }
 
-  public void editEventsFromTo(String subject, LocalDateTime startTime, LocalDateTime endTime) {
-    LocalDate startDate = startTime.toLocalDate();
-    LocalDate endDate = endTime.toLocalDate();
+  public void editEventSingle(String subject, LocalDateTime startTime, LocalDateTime endTime, String property, String newValue) {
+    // Handle edge case multiple date spanning event
+    Event foundEvent = searchEvent(subject, startTime, endTime);
+    if (foundEvent != null) {
+      removeEvent(foundEvent);
+      String newSubject = foundEvent.getSubject();
+      LocalDateTime newStartTime = foundEvent.getStartTime();
+      LocalDateTime newEndTime = foundEvent.getEndTime();
+      String newDescription = foundEvent.getDescription();
 
-
-    for (Map.Entry<String, Set<Event>> entry : calendar.entrySet()) {
-      LocalDate date = LocalDate.parse(entry.getKey());
-      if (( date.isEqual(startDate) || date.isAfter(startDate)) && (date.isEqual(endDate) ||date.isBefore(endDate))) {
-        Set<Event> events = entry.getValue();
-        for (Event event : events) {
-
+      switch (property) {
+        case "name": {
+          newSubject = newValue;
         }
+        case "startTime": {
+          newStartTime = LocalDateTime.parse(newValue);
+        }
+        case "endTime": {
+          newEndTime = LocalDateTime.parse(newValue);
+        }
+        case "description": {
+          newDescription = newValue;
+        }
+      }
+
+      try {
+        addEvent(newSubject, newDescription, newStartTime, newEndTime);
+      }
+      catch (Exception e) {
+        addEvent(foundEvent.getSubject(), foundEvent.getDescription(), foundEvent.getStartTime(), foundEvent.getEndTime());
       }
     }
   }
 
-  public void editEventsFrom(String subject, LocalDateTime startTime) {
+  public void editEventRecurring(String subject, LocalDateTime startTime, String property, String newValue) {
+    List<Event> events = searchEvents(subject, startTime);
+    if (!events.isEmpty()) {
+      for (Event event : events) {
+        removeEvent(event);
+      }
+      Event foundEvent = events.get(0);
+      String newSubject = foundEvent.getSubject();
+      LocalDateTime newStartTime = foundEvent.getStartTime();
+      LocalDateTime newEndTime = foundEvent.getEndTime();
+      String newDescription = foundEvent.getDescription();
+      LocalDateTime newEndRecurring = foundEvent.getEndRecurring();
+      String newRecurringDays = foundEvent.getRecurringDays();
+      int newOccurrences = foundEvent.getOccurrences();
 
+      switch (property) {
+        case "name": {
+          newSubject = newValue;
+        }
+        case "startTime": {
+          newStartTime = LocalDateTime.parse(newValue);
+        }
+        case "endTime": {
+          newEndTime = LocalDateTime.parse(newValue);
+        }
+        case "description": {
+          newDescription = newValue;
+        }
+        case "endRecurring": {
+          newEndRecurring = LocalDateTime.parse(newValue);
+        }
+        case "recurringDays": {
+          newRecurringDays = newValue;
+        }
+        case "occurrences": {
+          newOccurrences = Integer.parseInt(newValue);
+        }
+      }
+      try {
+        addRecurringEvents(newSubject, newDescription, newStartTime, newEndTime, newEndRecurring, newRecurringDays, newOccurrences);
+      }
+      catch (Exception e) {
+        addRecurringEvents(foundEvent.getSubject(), foundEvent.getDescription(), foundEvent.getStartTime(), foundEvent.getEndTime(), foundEvent.getEndRecurring(), foundEvent.getRecurringDays(), foundEvent.getOccurrences());
+      }
+    }
   }
 
-  public void editEvents(String subject) {
-
+  private void removeEvent(Event event) {
+    String date = dateFormatter.format(event.getStartTime());
+    Set<Event> events = calendar.get(date);
+    events.remove(event);
   }
 
-  public Event removeEvent() {}
+  private Event searchEvent(String subject, LocalDateTime startTime, LocalDateTime endTime) {
+    for (Map.Entry<String, Set<Event>> entry : calendar.entrySet()) {
+      Set<Event> events = entry.getValue();
+      for (Event event : events) {
+        if (event.getSubject().equals(subject) && event.getStartTime().equals(startTime) && event.getEndTime().equals(endTime)) {
+          return event;
+        }
+      }
+    }
+    return null;
+  }
 
-  public Event searchEvent(){}
+  private List<Event> searchEvents(String subject, LocalDateTime startTime) {
+    List<Event> foundEvents = new ArrayList<>();
+    for (Map.Entry<String, Set<Event>> entry : calendar.entrySet()) {
+      Set<Event> events = entry.getValue();
+      for (Event event : events) {
+        LocalDate currentDate = event.getStartTime().toLocalDate();
+        LocalDate searchDate = startTime.toLocalDate();
+        LocalTime currentTime = event.getStartTime().toLocalTime();
+        LocalTime searchTime = startTime.toLocalTime();
+        if (event.getSubject().equals(subject)) {
+          if (startTime != null) {
+            if ((currentDate.equals(searchDate) || currentDate.isAfter(searchDate)) && currentTime.equals(searchTime)) {
+              foundEvents.add(event);
+            }
+          } else {
+            foundEvents.add(event);
+          }
+        }
+      }
+    }
+    return foundEvents;
+  }
 
   public void addEvent(String subject, String description, LocalDateTime startTime, LocalDateTime endTime) {
     Event event = new Event(subject, description, startTime, endTime);
@@ -58,7 +153,7 @@ public class Calendar {
   }
 
   public void addRecurringEvents(String subject, String description, LocalDateTime startTime, LocalDateTime endTime,
-                                 LocalDateTime endRecurring, List<String> recurringDays, int occurrences) {
+                                 LocalDateTime endRecurring, String recurringDays, int occurrences) {
     List<Event> events = generateRecurringEvents(subject, description, startTime, endTime, endRecurring, recurringDays, occurrences);
     if (hasAnyConflict(events)) {
       throw new IllegalArgumentException("Recurring event series conflicts with existing events.");
@@ -92,7 +187,7 @@ public class Calendar {
   }
 
   private List<Event> generateRecurringEvents(String subject, String description, LocalDateTime startTime, LocalDateTime endTime,
-                                              LocalDateTime endRecurring, List<String> recurringDays, int occurrences) {
+                                              LocalDateTime endRecurring, String recurringDays, int occurrences) {
     List<Event> result = new ArrayList<>();
     LocalDateTime currentStartTime = startTime;
     LocalDateTime currentEndTime = endTime;
@@ -103,13 +198,16 @@ public class Calendar {
         break;
       }
 
-      if (recurringDays.contains(currentStartTime.getDayOfWeek().toString())) {
-        result.add(new Event(subject, description, currentStartTime, currentEndTime));
+      if (recurringDays.contains(currentStartTime.getDayOfWeek().toString())) { // Update for new enum to parse day of the week to characters
+        result.add(new Event(subject, description, currentStartTime, currentEndTime, endRecurring, recurringDays, occurrences));
         count++;
       }
 
       currentStartTime = currentStartTime.plusDays(1);
       currentEndTime = currentEndTime.plusDays(1);
+      if (occurrences > 0) {
+        occurrences--;
+      }
     }
 
     return result;
--- src/main/java/calendar/Event.java
+++ src/main/java/calendar/Event.java
@@ -1,12 +1,16 @@
 package calendar;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Event {
   private String subject;
   private LocalDateTime startTime;
   private LocalDateTime endTime;
   private String description;
+  private LocalDateTime endRecurring;
+  private String recurringDays;
+  private int occurrences;
 
   public Event(String subject, String description, LocalDateTime startTime, LocalDateTime endTime) {
     if (startTime == null) {
@@ -21,12 +25,48 @@ public class Event {
     this.endTime = endTime;
   }
 
+  public Event(String subject, String description, LocalDateTime startTime, LocalDateTime endTime, LocalDateTime endRecurring, String recurringDays, int occurrences) {
+    if (startTime == null) {
+      throw new IllegalArgumentException("Start time cannot be null.");
+    }
+    if (endTime != null && endTime.isBefore(startTime)) {
+      throw new IllegalArgumentException("End time cannot be before start time.");
+    }
+    this.subject = subject;
+    this.description = description;
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.endRecurring = endRecurring;
+    this.recurringDays = recurringDays;
+    this.occurrences = occurrences;
+  }
+
+  public String getSubject() {
+    return subject;
+  }
+
   public LocalDateTime getStartTime() {
     return startTime;
   }
 
   public LocalDateTime getEndTime() {
     return endTime;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public LocalDateTime getEndRecurring() {
+    return endRecurring;
+  }
+
+  public String getRecurringDays() {
+    return recurringDays;
+  }
+
+  public int getOccurrences() {
+    return occurrences;
   }
 
   public boolean isConflicted(Event otherEvent) {
