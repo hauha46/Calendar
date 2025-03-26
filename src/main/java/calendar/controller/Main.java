a5aa281e0e7d9a14d9d7a2dc2879320762c3a981
Anand283 <pinnamanenianand177@gmail.com>
2025-03-26 02:58:10 -0400

    adding main file for startu, java doc for test cases yet to be done

---
+++ src/main/java/calendar/controller/Main.java
@@ -0,0 +1,18 @@
+package controller;
+
+import calendar.controller.CommandController;
+import calendar.manager.CalendarManager;
+import calendar.view.Interpreter;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+  public static void main(String[] args) {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    Interpreter view = new Interpreter();
+    CalendarManager model = new CalendarManager();
+    CommandController controller = new CommandController(model,view);
+    controller.start();
+  }
+}
